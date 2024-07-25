71af5cec28a6390ecce62c2a7f9f9f4adaedeec8
Spiros <107565036+Sismspir@users.noreply.github.com>
2024-07-25 09:48:45 +0300

    Update index.js

--- social/server/index.js
+++ social/server/index.js
@@ -7,7 +7,7 @@ const port = 3000;
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Play16@@12@@',
+    password: 'pass',
     database: "social",
 });
 connection.connect((error) => {
